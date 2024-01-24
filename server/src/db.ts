094e9c8c09cb07cf49a613b41cf6c34be15e818e
Alberto Elias <hi@albertoelias.me>
2024-01-24 21:46:20 +0100

    server fixes

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,6 +1,6 @@
 import { DataSource } from 'typeorm'
 
-console.log(process.env)
+console.log(`${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`)
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DATABASE_URL,
